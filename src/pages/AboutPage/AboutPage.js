dc96f7952402c060e516ab74b0624a9619965edb
Gabriel <Gabrielpedrofc@gmail.com>
2024-05-10 21:41:47 -0300

    Roteamento

---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+
+function AboutPage() {
+  return (
+    <>
+Teste About!
+    </>
+  );
+}
+
+export default AboutPage;
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+
+function HomePage() {
+  return (
+    <>
+Teste Home!
+    </>
+  );
+}
+
+export default HomePage;
